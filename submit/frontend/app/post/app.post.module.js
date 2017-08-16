fc502b13bc1e12d789098c0910c8ceabb7dcbcf8
Gengwu Li <gengwuli@gmail.com>
2017-08-15 20:44:46 -0700

    third commit

--- submit/frontend/app/post/app.post.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    /**
-     * inject ngAnimate and ui.bootstrap to use modal and popover components
-     * post module is big, so separate it in a independent module
-     */
-    angular
-        .module('app.post', ['ngAnimate', 'ui.bootstrap']);
-})();
